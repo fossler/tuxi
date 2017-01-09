358937a1fa5ec194a69601b8178c4c8653391c7d
Mirzet Kadic <mirzet.kadic@gmail.com>
2017-01-09 16:16:18 +0100

    mask2cdr func added

--- tuxi.sh
+++ tuxi.sh
@@ -214,6 +214,18 @@ net_dns_srv (){
   fi
 }
 
+mask2cdr (){
+    # Assumes there's no "255." after a non-255 byte in the mask
+    if [[ $1 == --- ]]; then
+      echo "---"
+    else
+      local x=${1##*255.}
+      set -- 0^^^128^192^224^240^248^252^254^ $(( (${#1} - ${#x})*2 )) ${x%%.*}
+      x=${1%%$3*}
+      echo $(( $2 + (${#x}/4) ))
+    fi
+ }
+
 # Hardware & Ressources
 # ##############################################################
 
